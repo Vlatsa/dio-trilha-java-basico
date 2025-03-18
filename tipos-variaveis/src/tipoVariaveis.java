9721b15b80f9058b543b1ea95e3fdca53022980e
Vlatsa <vlatsa@yahoo.com.br>
2025-03-18 11:14:34 -0300

    Alterando nome do arquivo .java

---
+++ tipos-variaveis/src/tipoVariaveis.java
@@ -0,0 +1,31 @@
+public class tipoVariaveis {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        String nome = "Lucas"; 
+
+        char sexo = 'M'; // 0 a 65535
+
+        boolean ativo = true; // true ou false
+
+
+        float salarioFloat = 1000.00f; // 1.4E-45 a 3.4028235E38
+
+        double salario = 1000.00; // 4.9E-324 a 1.7976931348623157E308
+
+
+        byte idadeByte = 20; // -128 a 127
+
+        short idadeShort = 20; // -32.768 a 32.767
+
+        int idade = 20; // -2.147.483.648 a 2.147.483.647
+
+        long idadeLong = 20; // -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
+
+        //estudar
+            //Tipos Primitivos de Variaveis
+            //Classe String que representa texto na aplicação
+      
+
+    }
+}
