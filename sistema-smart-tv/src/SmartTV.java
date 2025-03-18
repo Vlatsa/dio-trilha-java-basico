4c1eea0e47f1e3c0348fb55fbb638f33045e172d
Vlatsa <vlatsa@yahoo.com.br>
2025-03-18 17:51:45 -0300

    Aula Metodos Concluida

--- sistema-smart-tv/src/SmartTV.java
+++ sistema-smart-tv/src/SmartTV.java
@@ -2,15 +2,40 @@ public class SmartTV {
     boolean ligada=false;
     int canal = 1;
     int volume = 25;
-    
+
+    public void mudarCanal(int novoCanal) {
+    canal = novoCanal;
+    System.err.println("Mudando Canal para: " + canal);
+    }
+
+    public void aumentarCanal() {
+    canal++;
+    System.err.println("Aumentando Canal para: " + canal);
+    }
+
+    public void diminuirCanal() {
+    canal--;
+    System.err.println("Diminuindo Canal para: " + canal);
+    }
+
+    public void aumentarvolume() {
+    volume++;
+    System.err.println("Aumentando Volume para: " + volume);
+    }
+
+    public void diminuirvolume() {
+    volume--;
+    System.err.println("Diminuindo Volume para: " + volume);
+    }
+
     public void ligar() {
-        ligada = true;
+    ligada = true;
     }
 
     public void desligar() {
-        ligada = false;
+    ligada = false;
     }
-
+    
     //public boolean isLigada() {
     //   return ligada;
     //}
--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -4,15 +4,36 @@ public class Usuario {
        
         SmartTV smartTv = new SmartTV();
 
+        smartTv.aumentarvolume();
+        smartTv.aumentarvolume();
+        smartTv.aumentarvolume();
+        System.out.println("Volume Atual: " + smartTv.volume);
+
+        smartTv.diminuirvolume();
+        System.out.println("Volume Atual: " + smartTv.volume);
+
         System.out.println("TV Ligada? " + smartTv.ligada);
         System.out.println("Canal Atual: " + smartTv.canal);
         System.out.println("Volume Atual: " + smartTv.volume);
-
+        
         smartTv.ligar();
-        System.out.println("TV Ligada? " + smartTv.ligada);
-        
+        System.out.println("Novo Status: TV Ligada? " + smartTv.ligada);
+
         smartTv.desligar();
-        System.out.println("TV Ligada? " + smartTv.ligada);
-        
+        System.out.println("Novo Status: TV Ligada? " + smartTv.ligada);
+
+        smartTv.aumentarCanal();
+        System.out.println("Canal Atual: " + smartTv.canal);
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        System.out.println("Canal Atual: " + smartTv.canal);
+        smartTv.diminuirCanal();
+        System.out.println("Canal Atual: " + smartTv.canal);
+
+        smartTv.mudarCanal(10);
+        System.out.println("Canal Atual: " + smartTv.canal);
+
+
     }
 }
